6d8426bf1355f5b4c58b28d5b16a86e81cd95dfa
valoooaltuho-gif <valoooaltuho@gmail.com>
2026-02-02 19:19:33 +0300

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from './fixtures';
 
-test('успешный переход в документацию', async ({ homePage,selectCntrAndLangPage, page }) => {
+test('small test', async ({ homePage,selectCntrAndLangPage, page }) => {
   await homePage.goto();
   await homePage.clickSelectCountry();
   await selectCntrAndLangPage.selectCountry("Россия");
