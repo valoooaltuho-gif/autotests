d3d3e5303f31f32dff0796f9a66559f255713de5
valoooaltuho-gif <valoooaltuho@gmail.com>
2026-02-02 19:20:49 +0300

    Update api-test.spec.ts

--- tests/api-test.spec.ts
+++ tests/api-test.spec.ts
@@ -1,24 +1,19 @@
 import { test, expect } from './fixtures';
 
-test('получение токена статус 200', async ({ userClient }) => {
+test('get token with status 200', async ({ userClient }) => {
   const response = await userClient.getSecretAndRefresh();
   
-  // Проверяем статус
   expect(response.status()).toBe(200);
   
-  // Проверяем тело ответа
   const body = await response.json();
-  // 2. Проверяем структуру и значения
   expect(body).toMatchObject({
     success: true,
     data: {
-      // Проверка, что это строки и они не пустые
       token: expect.any(String),
       refresh: expect.any(String)
     }
   });
 
-  // 3. Дополнительная проверка на длину или наличие символов (если нужно)
   expect(body.data.token.length).toBeGreaterThan(0);
   expect(body.data.refresh).not.toBe('');
 });
